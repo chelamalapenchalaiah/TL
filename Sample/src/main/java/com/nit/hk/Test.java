aae973329b971081ef0781a74d3d3bec37a6c2b9
HP <HP@chanti>
2025-01-10 12:49:06 +0530

    commit

--- Sample/src/main/java/com/nit/hk/Test.java
+++ Sample/src/main/java/com/nit/hk/Test.java
@@ -3,7 +3,10 @@ package com.nit.hk;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hiii hello");
-		
+		m1();
+	}
+	static void m1() {
+		System.out.println("proj1-B1-payment");
 	}
 
 }
