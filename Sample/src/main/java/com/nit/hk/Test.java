bd755d6d072daff745a42e191bdd9ed96d39fa65
HP <HP@chanti>
2025-01-10 13:01:55 +0530

    add two methods

--- Sample/src/main/java/com/nit/hk/Test.java
+++ Sample/src/main/java/com/nit/hk/Test.java
@@ -4,8 +4,16 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hiii hello");
 		m1();
+		m2();
+		m3();
 	}
 	static void m1() {
+		System.out.println("proj1-B1-payment");
+	}
+	static void m2() {
+		System.out.println("proj1-B1-payment");
+	}
+	static void m3() {
 		System.out.println("proj1-B1-payment");
 	}
 
